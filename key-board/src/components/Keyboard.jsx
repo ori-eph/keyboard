7962175d8f03a1c38736e5bf07e172e0ed282a61
oopall <opalamsalem@hilma.tech>
2023-11-12 12:30:33 +0200

    fix

--- key-board/src/components/Keyboard.jsx
+++ key-board/src/components/Keyboard.jsx
@@ -1,32 +1,32 @@
 import { useState } from "react";
-import {lang} from "../data" 
+import { lang } from "../data";
 import Button from "./Button";
 
-function findLangChars(currentLang){
-    for (key in lang){
-        if (key===currentLang){
-            return lang[key];
-        }
+function findLangChars(currentLang) {
+  for (key in lang) {
+    if (key === currentLang) {
+      return lang[key];
     }
+  }
 }
 
-function onKeyClick=(setText, char)=>{
-    setText(prevText=>prevText+=char);
+function onKeyClick(setText, char) {
+  setText((prevText) => (prevText += char));
 }
 
-function getKeyboard(chars, setText){
-    const keyboard=chars.map(char=><Button key={`key-${char}`} onClick={()=>onKeyClick(setText, {char})}>{char}</Button>);
+function getKeyboard(chars, setText) {
+  const keyboard = chars.map((char) => (
+    <Button key={`key-${char}`} onClick={() => onKeyClick(setText, { char })}>
+      {char}
+    </Button>
+  ));
 }
 
 function Keyboard(props) {
-    const currentLang=props.lang||'eng';
-    const chars=findLangChars(currentLang);
-    const keyboard=getKeyboard(chars, props.setText);
-    return ( 
-        <div id='keyboard-flex'>
-            {keyboard}
-        </div>
-     );
+  const currentLang = props.lang || "eng";
+  const chars = findLangChars(currentLang);
+  const keyboard = getKeyboard(chars, props.setText);
+  return <div id="keyboard-flex">{keyboard}</div>;
 }
 
 export default Keyboard;
