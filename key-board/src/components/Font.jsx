5abc0d556f9d2dadaa8130f43aba8812adac0004
oopall <opalamsalem@hilma.tech>
2023-11-13 10:56:10 +0200

    finished

--- key-board/src/components/Font.jsx
+++ key-board/src/components/Font.jsx
@@ -1,30 +1,46 @@
 import Button from "./Button";
 import "../css/keyboard.css";
 import { fonts } from "../data";
+import { useState } from "react";
 
 export default function Font(props) {
+  const [styleB, setStyleB] = useState("");
+  const [styleI, setStyleI] = useState("");
+  const [styleU, setStyleU] = useState("");
+
   function handleFontStyleChange(e) {
-    props.fontStyle !== e
-      ? props.setFontStyle(e)
-      : props.setFontStyle("normal");
+    if (props.fontStyle !== e) {
+      props.setFontStyle(e);
+      setStyleI(" pressed");
+    } else {
+      props.setFontStyle("normal");
+      setStyleI("");
+    }
   }
 
   function handleFontWeightChange(e) {
-    props.fontWeight !== e
-      ? props.setFontWeight(e)
-      : props.setFontWeight("normal");
+    if (props.fontWeight !== e) {
+      props.setFontWeight(e);
+      setStyleB(" pressed");
+    } else {
+      props.setFontWeight("normal");
+      setStyleB("");
+    }
   }
 
   function handleTextDecorationChange(e) {
-    props.textDecoration !== e
-      ? props.setTextDecoration(e)
-      : props.setTextDecoration("none");
+    if (props.textDecoration !== e) {
+      setStyleU(" pressed");
+      props.setTextDecoration(e);
+    } else {
+      props.setTextDecoration("none");
+      setStyleU("");
+    }
   }
 
   function handleFontFamChange(e) {
     props.setFontFam(e.target.value);
   }
-
   return (
     <div className="Font tool">
       <label htmlFor="fontFam">font: &nbsp;</label>
@@ -43,20 +59,17 @@ export default function Font(props) {
       <br />
       <Button
         value="bold"
-        className="key fontKey"
-        style={{ fontWeight: "bold" }}
+        className={`key fontKey bold${styleB}`}
         onClick={() => handleFontWeightChange("bold")}
       />
       <Button
         value="italic"
-        className="key fontKey"
-        style={{ fontStyle: "italic" }}
+        className={`key fontKey italic${styleI}`}
         onClick={() => handleFontStyleChange("italic")}
       />
       <Button
         value="underline"
-        className="key fontKey"
-        style={{ textDecoration: "underline" }}
+        className={`key fontKey underline${styleU}`}
         onClick={() => handleTextDecorationChange("underline")}
       />
     </div>
